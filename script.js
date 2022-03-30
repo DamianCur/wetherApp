cf364b8d782c47798bc5dd39b179960d73da31a5
xoriston <1420damian@gmail.com>
2022-03-30 20:37:20 +0200

    coded url logic

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ const photo = document.querySelector('.photo')
 const weather = document.querySelector('.weather')
 const temperature = document.querySelector('.temperature')
 const humidity = document.querySelector('.humidity')
+
+
+const apiLink = "https://api.openweathermap.org/data/2.5/weather?q="
+const apiKey = '&appid=fe2c049daff6932a9c53869c926ddc78'
+const apiUnits = '&units=metric'
+
+const getWether = () => {
+    const city = input.value
+    const URL = `${apiLink}${city}${apiKey}${apiUnits}`
+}
