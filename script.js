dcf866d44ab169cffa1c36eb8b3b80e38c5bb509
xoriston <1420damian@gmail.com>
2022-03-30 22:21:29 +0200

    added weather parameters

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const cityName = document.querySelector('.city-name')
 const warning = document.querySelector('.warning')
 const photo = document.querySelector('.photo')
 const weather = document.querySelector('.weather')
-const temperature = document.querySelector('.temperature')
-const humidity = document.querySelector('.humidity')
+const temperatureText = document.querySelector('.temperature')
+const humidityText = document.querySelector('.humidity')
 
 
 const apiLink = "https://api.openweathermap.org/data/2.5/weather?q="
@@ -13,6 +13,21 @@ const apiKey = '&appid=fe2c049daff6932a9c53869c926ddc78'
 const apiUnits = '&units=metric'
 
 const getWether = () => {
-    const city = input.value
+    const city = input.value || 'Warsaw'
     const URL = `${apiLink}${city}${apiKey}${apiUnits}`
+
+    axios.get(URL).then(res => {
+
+        const wetherData = res.data
+        console.log(wetherData);
+        const temperatureValue = wetherData.main.temp.toFixed()
+        const humidityValue = wetherData.main.humidity
+
+        cityName.textContent = wetherData.name
+        temperatureText.textContent = temperatureValue + "°C"
+        humidityText.textContent = humidityValue + "%"
+
+    })
 }
+
+getWether()
