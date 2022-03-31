d4e40ce542c2b02229151a4e54fdbffd906b2c94
xoriston <1420damian@gmail.com>
2022-03-31 19:44:34 +0200

    added switch to weather img | finished app

--- script.js
+++ script.js
@@ -18,16 +18,64 @@ const getWether = () => {
 
     axios.get(URL).then(res => {
 
+        warning.textContent = ""
+
         const wetherData = res.data
         console.log(wetherData);
         const temperatureValue = wetherData.main.temp.toFixed()
         const humidityValue = wetherData.main.humidity
+        const status = Object.assign({}, ...wetherData.weather)
+       
 
         cityName.textContent = wetherData.name
         temperatureText.textContent = temperatureValue + "°C"
         humidityText.textContent = humidityValue + "%"
+        weather.textContent = status.main
 
+
+        switch (true) {
+            case 232 > status.id:
+                photo.setAttribute('src', '/img/thunderstorm.png')
+                break
+
+            case 300 <= status.id && 321 >= status.id:
+                photo.setAttribute('src', '/img/dizzle.png')
+                break
+
+            case 500 <= status.id && 351 >= status.id:
+                photo.setAttribute('src', '/img/rain.png')
+                break
+
+            case 600 <= status.id && 622 >= status.id:
+                photo.setAttribute('src', '/img/ice.png')
+                break
+
+            case 741 === status.id:
+                photo.setAttribute('src', '/img/fog.png')
+                break
+
+            case 800 === status.id:
+                photo.setAttribute('src', './img/sun.png')
+                break
+
+            case 801 <= status.id:
+                photo.setAttribute('src', './img/cloud.png')
+                break
+
+            default:
+                photo.setAttribute('src', '/img/unknown.png')
+                break
+        }
+
+    }).catch(() => {
+        warning.textContent = "Incorrect city name!"
     })
 }
 
-getWether()
+const enterCheck = (e) => {
+    if(e.key === "Enter") {
+        getWether()
+    }
+}
+input.addEventListener("keyup", enterCheck)
+button.addEventListener("click", getWether)
